0a9ba05fed9b6b4805d4324a925b63ded08a5c54
Arthurmeade12 <s728c3ilp@relay.firefox.com>
2023-08-16 18:44:15 -0500

    Completely redo update_purpur.sh

--- update_purpur.sh
+++ update_purpur.sh
@@ -1,143 +1,133 @@
 #!/usr/bin/env bash
+#shellcheck disable=2164,2317
+#shellcheck shell=bash
+#shellcheck enable=require-variable-braces,require-double-brackets
+set -u
 SOURCE=${SOURCE:="$(dirname "${0}")/config.sh"}
 NOHASHUPDATE=${NOHASHUPDATE:='false'} # If true, will download every time if there's an update or not.
-INTOUPDATE=${INTOUPDATE:='false'} # If true, will download updates into the 'update' folder.
+INTOUPDATE=${INTOUPDATE:='true'} # If true, will download updates into the 'update' folder.
+#shellcheck source-path=SCRIPTDIR
+#shellcheck source=config.sh
 . "${SOURCE}"
-export GH_TOKEN="$(cat "${MC_DIR}"/gh_token)"
+GH_TOKEN="$(cat "${MC_DIR}"/gh_token)"
+export GH_TOKEN
 badfile(){
-    warn "Download of ${1} failed, or file did not match hash. \nTry manually downloading or restoring file from ~/.local/share/trash/files ."
+  warn "Download of ${1} failed, or file did not match hash. \nTry manually downloading or restoring file from ~/.local/share/trash/files ."
 }
 intoupdate(){
-    [[ "${INTOUPDATE}" = 'true' ]] || return
-    if [[ "$(basename "$(pwd)")" = 'update' ]]
-    then
-        popd
-    else
-        pushd_noerror "${MC_DIR}/plugins/update"
-    fi
+  [[ "${INTOUPDATE}" = 'true' ]] || return
+  if [[ "$(basename "$(pwd)")" = 'update' ]]
+  then
+    popd
+  else
+    pushd_noerror "${MC_DIR}/plugins/update"
+  fi
 }
 skipnohashupdate(){
-    if [[ "${NOHASHUPDATE}" = 'false' ]]
-    then
-        out "Skipping ${1} because \$NOHASHUPATE = false."
-        return 1
-    fi
+  if [[ "${NOHASHUPDATE}" = 'false' ]]
+  then
+    out "Skipping ${1} because \$NOHASHUPATE = false."
+    return 1
+  fi
 }
 consturl(){
-    skipnohashupdate "${1}" || return 0
-    intoupdate
-    if curl -JLOf# --clobber "${2}"
-    then
-      out "Successfully downloaded latest version of ${1}!"
-    else
-        warn "Unable to download ${1}. Check your internet connection. "
-    fi
-    intoupdate
+  skipnohashupdate "${1}" || return 0
+  if curl -JLOf# --clobber "${2}"
+  then
+    out "Successfully downloaded latest version of ${1}!"
+  else
+    warn "Unable to download ${1}. Check your internet connection. "
+  fi
 }
 githubeval(){
-    skipnohashupdate "${1}" || return 0
-    local REPO="${1}"
-    local NAME="${1#*/}"
-    local JQCOMMAND=".[].assets[] | select(startswith(\"${2%'*.jar'}\") | .browser_download_url)"
-    intoupdate
-    if curl -JLOf# --clobber \
+  skipnohashupdate "${1}" || return 0
+  local -r REPO="${1}" NAME="${1#*/}"
+  #local -r JQCOMMAND=".[].assets[] | select(startswith(\"${2%'*.jar'}\") | .browser_download_url)"
+  if curl -JLOf# --clobber \
     "$(gh api --header 'Accept: application/vnd.github+json' --method GET "/repos/${REPO}/releases" -F per_page=1 |\
-        jq -r '.[].assets[].browser_download_url')"
-    then
-        out "Successfully downloaded latest version of ${1}!"
-    else
-        warn "Unable to download ${NAME}. Check your internet connection. "
-    fi
-    intoupdate
+      jq -r '.[].assets[].browser_download_url')"
+  then
+    out "Successfully downloaded latest version of ${1}!"
+  else
+    warn "Unable to download ${NAME}. Check your internet connection. "
+  fi
 }
 nofilefake(){
-    [[ -z "${FILE}" ]] && \
-        export FILE="$(mktemp)" 
+  if [[ -z "${FILE}" ]]
+  then
+    FILE="$(mktemp)"
+    export FILE
+  fi
 }
 geysereval(){
-    intoupdate
-	local FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)"
-    local APIURL="https://download.geysermc.org/v2/projects/${1}/versions/latest/builds/latest"
-    local NAME="${1^}"
-    nofilefake
-    geysercheckintegrity(){
-        [[ "$(curl --silent -L "${APIURL}" | jq -r .downloads.spigot.sha256)" = \
-        "$(sha256sum "${FILE}" | cut -d " " -f 1 )" ]] 
-    }
+  local FILE \
+    APIURL="https://download.geysermc.org/v2/projects/${1}/versions/latest/builds/latest" \
+    NAME="${1^}"
+  FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)"
+  nofilefake
+  geysercheckintegrity(){
+    [[ "$(curl --silent -L "${APIURL}" | jq -r .downloads.spigot.sha256)" = \
+      "$(sha256sum "${FILE}" | cut -d " " -f 1 )" ]]
+  }
 	if geysercheckintegrity
 	then
 		out "${NAME} is up to date!"
 	else
 		curl -#L -o "$(curl --silent -L "${APIURL}" |\
-            jq -r .downloads.spigot.name)" "${APIURL}"/downloads/spigot
-        if geysercheckintegrity
-		then
-            out "Updated ${NAME}"
-        else
-            badfile
-        fi
+      jq -r .downloads.spigot.name)" "${APIURL}"/downloads/spigot
+    FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)" # Redefine for geysercheckintegrity
+    if geysercheckintegrity
+	then
+      out "Updated ${NAME}"
+    else
+      badfile "${NAME}"
     fi
-	intoupdate
-}
-spigeteval(){
-    intoupdate
-    local FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)"
-    local APIURL="https://api.spiget.org/v2/resources/${3}}/download"
-    local NAME="${1^}"
-    skipnohashupdate "${1}" || return 0
-    curl -JLO# 
+  fi
 }
 mreval(){
-    intoupdate
-    local FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)"
-    local APIURL="https://api.modrinth.com/v2/version_file/${3}/update" # Uses sha1sum of a version of ${1}
-    local JQCOMMAND=".files[] | select(.filename | startswith(\"${2%'*.jar'}\"))"
-    mrdown(){
-        curl -fLX POST --silent "${1}" -H "Content-Type: application/json" --data-binary @- <<DATA
+  local FILE \
+    APIURL="https://api.modrinth.com/v2/version_file/${3}/update" \
+    JQCOMMAND=".files[] | select(.filename | startswith(\"${2%'*.jar'}\"))"
+  FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)"
+  mrdown(){
+    curl -fLX POST --silent "${1}" -H "Content-Type: application/json" --data-binary @- <<DATA
 {
   "loaders": [
       "paper",
       "purpur"
   ],
   "game_versions": [
-    "1.20.1",
-    "1.20",
-    "1.19.4",
-    "1.19.3",
-    "1.19.2",
-    "1.19.1",
-    "1.19"
-  ],
-  "algorithm":"sha1"
+    "1.20.1"
+  ]
 }
 DATA
-    }
-    mrcheckintegrity(){
-        [[ "$(mrdown "${APIURL}" | jq -r "${JQCOMMAND} | .hashes.sha1")" = \
-        "$(sha1sum "${FILE}" | head -c 40)" ]]
-    }
-    nofilefake
+  }
+  mrcheckintegrity(){
+    [[ "$(mrdown "${APIURL}" | jq -r "${JQCOMMAND} | .hashes.sha1")" = \
+      "$(sha1sum "${FILE}" | head -c 40)" ]]
+  }
+  nofilefake
+  if mrcheckintegrity
+  then
+    out "${1} is up to date!"
+  else
+    trash "${FILE}"
+    curl -JLOf# "$(mrdown "${APIURL}" | \
+      jq -r "${JQCOMMAND} | .url")"
+    FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)" # We need to redefine since filename has changed.
     if mrcheckintegrity
     then
-        out "${1} is up to date!"
+      out "Updated ${1}"
     else
-        trash "${FILE}"
-       	curl -JLOf# "$(mrdown "${APIURL}" | jq -r "${JQCOMMAND} | .url")"
-        local FILE="$(find . -maxdepth 1 -name "${2}" 2>/dev/null)" # We need to redefine since filename has changed.
-        if mrcheckintegrity
-        then
-            out "Updated ${1}"
-        else
-            badfile "${1}"
-        fi
+      badfile "${1}"
     fi
-   intoupdate
+  fi
 }
 
 if ! nc -zw1 google.com 443 &>/dev/null
 then
-    abort "No internet connection."
-    exit 3
+  abort "No internet connection."
+  exit 3
 fi
 ##
 ## Update begins
@@ -150,58 +140,34 @@ PURPUROLDSUM="$(md5sum - < "${PURPURFILE}" | tr -d '  ' | tr -d  '-')"
 
 if [[ "${PURPURREMOTESUM}" = "${PURPUROLDSUM}" ]]
 then
-    out "Purpur is up to date!"
+  out "Purpur is up to date!"
 else
-    trash "${PURPURFILE}"
-    curl -JLO# "https://api.purpurmc.org/v2/purpur/${MINECRAFTVERSION}/latest/download"
-    # Re-eval for new jar name
-    PURPURFILE="$(find "${MC_DIR}" -maxdepth 1 -name "*.jar")"
-    PURPUROLDSUM="$(md5sum - < "${PURPURFILE}" | tr -d '  ' | tr -d '-')"
-    if [[ "${PURPUROLDSUM}" = "${PURPURREMOTESUM}" ]]
-    then
-        out 'Updated Purpur'
-    else
-        badfile "${1}"
-    fi
+  trash "${PURPURFILE}"
+  curl -JLO# "https://api.purpurmc.org/v2/purpur/${MINECRAFTVERSION}/latest/download"
+  # Re-eval for new jar name
+  PURPURFILE="$(find "${MC_DIR}" -maxdepth 1 -name "*.jar")"
+  PURPUROLDSUM="$(md5sum - < "${PURPURFILE}" | tr -d '  ' | tr -d '-')"
+  if [[ "${PURPUROLDSUM}" = "${PURPURREMOTESUM}" ]]
+  then
+    out 'Updated Purpur'
+  else
+    badfile "${1}"
+  fi
 fi
-[[ ! -d "${MC_DIR}"/plugins ]] && \
-	mkdir -p "${MC_DIR}"/plugins
-pushd_noerror "${MC_DIR}"/plugins
-#shellcheck disable=SC2164
-githubeval 'zeshan321/ActionHealth' 'ActionHealth-*.jar'
-mreval 'Chunky' 'Chunky-*.jar' 'd57bb3ab4277aa61501aba80a2de5d111bed7f26'
-mreval 'ChunkyBorder' 'ChunkyBorder-*.jar' '991471b1639beb70b006aa3b9e4d9a163428ea2e'
-mreval 'CoreProtect' 'CoreProtect-*.jar' 'f36771209f036c4c1899381e16acc65cbb7a07a0'
+pushd_noerror "${MC_DIR}/plugins"
+[[ "${INTOUPDATE}" = 'true' ]] && \
+  pushd_noerror './update'
+mreval 'CoreProtect' 'CoreProtect-*.jar' '7804652e9dd07410a071f7d1388ed89bd29b165c'
 warn 'Skipping CraftBook!' # Only available on bukkit.org (curseforge api)
-consturl 'CrossPlatforms' 'https://ci.kejonamc.dev/job/CrossplatForms/job/main/lastSuccessfulBuild/artifact/spigot/build/libs/CrossplatForms-Spigot.jar' # Jenkins, no way to get hash
-warn 'Skipping DeadChest!' # bukkit.org
-mreval 'EssentialsX' 'EssentialsX-*.jar' '1142297fc1facd0f6015202b21796a99571f681d'
-mreval 'EssentialsXChat' 'EssentialsXChat-*.jar' '1142297fc1facd0f6015202b21796a99571f681d'
-mreval 'EssentialsXGeoIP' 'EssentialsXGeoIP-*.jar' '1142297fc1facd0f6015202b21796a99571f681d'
+mreval 'EssentialsX' 'EssentialsX-*.jar' 'e626f9f250470bcf2feffbc3740f738f9cee50ef'
 geysereval 'floodgate' 'floodgate-spigot.jar'
-mreval 'FreedomChat' 'FreedomChat-*.jar' 'a8f48567c1ae58ae243043f2c5c914fd4dc6d76d'
 geysereval 'geyser' 'Geyser-Spigot.jar'
-githubeval 'Camotoy/GeyserSkinManager' 'GeyserSkinManager-Spigot.jar'
-warn 'Skipping Luckperms, and Multiverse-NetherPortals!'
-githubeval 'Multiverse/Multiverse-Core' 'multiverse-core-*.jar'
-githubeval 'Multiverse/Multiverse-Portals' 'multiverse-portals-*.jar'
-githubeval 'Multiverse/Multiverse-Inventories' 'multiverse-inventories-*.jar'
-githubeval 'Multiverse/Multiverse-SignPortals' 'multiverse-signportals-*.jar'
-mreval 'Pl3x Map' 'Pl3xMap-*.jar' '3803837eede294e30c43e455166dc88aedbd0c91'
-warn 'Skipping Placeholder API!' # spigot.org
-consturl 'ProtocolLib' 'https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar'
-mreval 'Purpur Extras' 'PurpurExtras-*.jar' 'b660e25faaed4e03082c2590aa3cd77147d7fdcf'
-mreval 'SchematicUpload' 'SchematicUpload-*.jar' '2727135c248e4ee5134057c65d0005a004feb4b6'
-githubeval 'Dans-Plugins/SimpleSkills' 'SimpleSkills-*.jar'
-mreval 'Simple Voice Chat' 'voicechat-bukkit-*.jar' '8450fce54dc83a4c8fad0e9755999dd7b78530a9'
-mreval 'Terra' 'Terra-*.jar' '3937d24a68a5efc9acfecbe67c78d026cd7ae5a8'
-#warn "Skipping Terra, since we are using a custom build."
-githubeval 'MilkBowl/Vault'
-githubeval 'ViaVersion/ViaVersion' 'ViaVersion-*.jar'
-githubeval 'ViaVersion/ViaBackwards' 'ViaBackwards-*.jar'
-githubeval 'MrMicky-FR/WorldEditSelectionVisualizer' 'WorldEditSelectionVisualizer-*.jar'
+warn 'Skipping Luckperms!'
+mreval 'Pl3x Map' "Pl3xMap-${MINECRAFTVERSION}-*.jar" 'e1243130008327413a1ff4b927d13f6306d11495'
+githubeval 'MilkBowl/Vault' 'Vault.jar'
 warn 'Skipping WorldEdit!' # bukkit.org
-mreval 'Xaero Map Spigot' 'xaero-map-*.jar' '99adb88c6749d0d7dff193bb2986c39289d5e19f'
 out 'Update complete!'
+[[ "$(dirname "$(pwd)")" = 'update' ]] && \
+  popd
 popd
 exit 0
