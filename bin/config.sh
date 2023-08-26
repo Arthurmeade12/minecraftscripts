0da0423a1d11e2336e9e6d07ba9768d061a5a4c0
Arthurmeade12 <s728c3ilp@relay.firefox.com>
2023-08-26 16:31:57 -0500

    Switch to aikar

--- bin/config.sh
+++ bin/config.sh
@@ -12,6 +12,6 @@ MINECRAFTVERSION=1.20.1 # Affects updating.
 MCRCON_PASS=CfBwZG3XA # Password for rcon.
 MCRCON_PORT=30001 # Port for rcon.
 OPTS="--nogui --log-strip-color -b ${MC_CONFIG}/bukkit.yml -C ${MC_CONFIG}/commands.yml -S ${MC_CONFIG}/spigot.yml --paper-dir ${MC_CONFIG}/paper/ --purpur ${MC_CONFIG}/purpur.yml  -W ${MC_DIR}/universe3 -c ${MC_CONFIG}/server.properties -d yyyy.MM.dd at HH:mm:ss -s 40 --server-name Arthurs_Server" # Opts to pass to the minecraft jar on the cli.
-JAVAFLAGSCHOICE='brucethemoose' # Can be 'brucethemoose' for Bruce's flags, 'aikar' for Aikar's flags, or zgc for standard ZGC flags.
+JAVAFLAGSCHOICE='aikar' # Can be 'brucethemoose' for Bruce's flags, 'aikar' for Aikar's flags, or zgc for standard ZGC flags.
 
 # End of file.
